2ab11ed1ba9f66a876433ebec3eac99cff23a9e4
Idriss Neumann <idriss.neumann@comwork.io>
2022-10-05 10:27:08 +0100

    Add slack notif

--- ci/compute-env.sh
+++ ci/compute-env.sh
@@ -4,3 +4,8 @@ git pull origin "${CI_COMMIT_BRANCH}" || :
 
 export IMAGE_NAME="uprodit_wiki"
 export VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
+
+# Slack
+export SLACK_USERNAME="uprodit_doc"
+export SLACK_EMOJI=":uprodit:"
+export SLACK_CHANNEL="#uprodit-prod"
